2d667e9a08e1e53567f64e722078f07985dc7645
LeaYeh <smile2140@gmail.com>
2024-04-01 11:34:51 +0200

    feat: Add lambda function exmaple in ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,29 +6,34 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 10:35:09 by lyeh              #+#    #+#             */
-/*   Updated: 2024/04/01 11:10:00 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/04/01 11:29:11 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include <algorithm>
-#include <cstring>
 
-void	print_upper_string(std::string str)
+void print_upper_string(std::string str)
 {
-	std::transform(str.begin(), str.end(), str.begin(), ::toupper);
-	std::cout << str;
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    std::cout << str;
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
-	std::for_each(argv + 1, argv + argc, print_upper_string);
-	std::cout << std::endl;
-	return (0);
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    // Transform each command-line argument to uppercase and print with lambda function
+    // std::for_each(argv + 1, argv + argc, [](char* arg) {
+    //     std::string str(arg);
+    //     std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    //     std::cout << str;
+    // });
+    std::for_each(argv + 1, argv + argc, print_upper_string);
+    std::cout << std::endl;
+    return (0);
 }
