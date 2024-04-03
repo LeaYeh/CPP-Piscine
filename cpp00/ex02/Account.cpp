83a9ec3cf2acd8d66519bb84e4ff32a420e8d84c
LeaYeh <smile2140@gmail.com>
2024-04-03 17:29:42 +0200

    feat: Implement the cpp00_ex02 but need to study the test.cpp

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 13:41:20 by lyeh              #+#    #+#             */
+/*   Updated: 2024/04/03 17:00:59 by lyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ctime>
+#include <iostream>
+#include "Account.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(void){};
+
+Account::Account(int initial_deposit)
+{
+    this->_amount = initial_deposit;
+    this->_nbDeposits = 0;
+    this->_nbWithdrawals = 0;
+    this->_accountIndex = _nbAccounts;
+    _nbAccounts++;
+    _totalAmount += this->_amount;
+    _displayTimestamp();
+    std::cout << "index: " << this->_accountIndex << "; amount: " << this->_amount << "; created" << std::endl;
+}
+
+Account::~Account(void)
+{
+    _displayTimestamp();
+    std::cout << "index: " << this->_accountIndex << "; amount: " << this->_amount << "; closed" << std::endl;
+}
+
+void Account::_displayTimestamp(void)
+{
+    std::time_t now = std::time(NULL);
+    char format_string[20];
+    std::strftime(format_string, sizeof(format_string), "%Y-%m-%dT%H:%M:%S", std::localtime(&now));
+    std::cout << "[" << format_string << "] ";
+}
+
+int Account::getNbAccounts(void)
+{
+    return _nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+    return _totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+    return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+    return _totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos(void)
+{
+    _displayTimestamp();
+    std::cout << "accounts:" << _nbAccounts
+              << "; total:" << _totalAmount
+              << "; deposits:" << _totalNbDeposits
+              << "; withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void Account::makeDeposit(int deposit)
+{
+    _displayTimestamp();
+    std::cout << "index:" << this->_accountIndex
+              << "; action: deposit"
+              << "; amount: " << this->_amount << " + " << deposit << " = " << (this->_amount + deposit) << std::endl;
+    this->_nbDeposits++;
+    _totalNbDeposits++;
+    _totalAmount += deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+    _displayTimestamp();
+    if (withdrawal > this->_amount)
+    {
+        std::cout << "index:" << this->_accountIndex << "; action: withdrawal; withdrawal refused" << std::endl;
+        return false;
+    }
+    std::cout << "index: " << this->_accountIndex
+              << "; amount: " << this->_amount << " - " << withdrawal << " = " << (this->_amount - withdrawal) << std::endl;
+    this->_nbWithdrawals++;
+    _totalNbWithdrawals++;
+    _totalAmount -= withdrawal;
+    return true;
+}
+
+int Account::checkAmount(void) const
+{
+    return this->_amount;
+}
+
+void Account::displayStatus(void) const
+{
+    _displayTimestamp();
+    std::cout << "index:" << this->_accountIndex
+              << "; amount:" << this->_amount
+              << "; deposits:" << this->_nbDeposits
+              << "; withdrawals:" << this->_nbWithdrawals << std::endl;
+}
