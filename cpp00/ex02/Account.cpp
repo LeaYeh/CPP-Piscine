20f22c76ec26b870a2b9a2447f4ac3dfb45d322e
LeaYeh <smile2140@gmail.com>
2024-04-04 11:44:54 +0200

    fix: Fix the log format need to perfectly match

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 13:41:20 by lyeh              #+#    #+#             */
-/*   Updated: 2024/04/03 17:00:59 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/04/04 11:42:52 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,20 @@ Account::Account(int initial_deposit)
     _nbAccounts++;
     _totalAmount += this->_amount;
     _displayTimestamp();
-    std::cout << "index: " << this->_accountIndex << "; amount: " << this->_amount << "; created" << std::endl;
+    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
 }
 
 Account::~Account(void)
 {
     _displayTimestamp();
-    std::cout << "index: " << this->_accountIndex << "; amount: " << this->_amount << "; closed" << std::endl;
+    std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 }
 
 void Account::_displayTimestamp(void)
 {
     std::time_t now = std::time(NULL);
-    char format_string[20];
-    std::strftime(format_string, sizeof(format_string), "%Y-%m-%dT%H:%M:%S", std::localtime(&now));
+    char format_string[16];
+    std::strftime(format_string, sizeof(format_string), "%Y%m%d_%H%M%S", std::localtime(&now));
     std::cout << "[" << format_string << "] ";
 }
 
@@ -71,17 +71,20 @@ void Account::displayAccountsInfos(void)
 {
     _displayTimestamp();
     std::cout << "accounts:" << _nbAccounts
-              << "; total:" << _totalAmount
-              << "; deposits:" << _totalNbDeposits
-              << "; withdrawals:" << _totalNbWithdrawals << std::endl;
+              << ";total:" << _totalAmount
+              << ";deposits:" << _totalNbDeposits
+              << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void Account::makeDeposit(int deposit)
 {
     _displayTimestamp();
     std::cout << "index:" << this->_accountIndex
-              << "; action: deposit"
-              << "; amount: " << this->_amount << " + " << deposit << " = " << (this->_amount + deposit) << std::endl;
+              << ";p_amount:" << this->_amount
+              << ";deposit:" << deposit
+              << ";amount:" << (this->_amount + deposit)
+              << ";nb_deposits:" << this->_nbDeposits + 1 << std::endl;
+    this->_amount += deposit;
     this->_nbDeposits++;
     _totalNbDeposits++;
     _totalAmount += deposit;
@@ -92,11 +95,18 @@ bool Account::makeWithdrawal(int withdrawal)
     _displayTimestamp();
     if (withdrawal > this->_amount)
     {
-        std::cout << "index:" << this->_accountIndex << "; action: withdrawal; withdrawal refused" << std::endl;
+        std::cout << "index:" << this->_accountIndex
+                  << ";p_amount:" << this->_amount
+                  << ";withdrawal:refused" << std::endl;
         return false;
     }
-    std::cout << "index: " << this->_accountIndex
-              << "; amount: " << this->_amount << " - " << withdrawal << " = " << (this->_amount - withdrawal) << std::endl;
+    std::cout << "index:" << this->_accountIndex
+              << ";p_amount:" << this->_amount
+              << ";withdrawal:" << withdrawal
+              << ";amount:" << (this->_amount - withdrawal)
+              << ";nb_withdrawals:" << this->_nbWithdrawals + 1
+              << std::endl;
+    this->_amount -= withdrawal;
     this->_nbWithdrawals++;
     _totalNbWithdrawals++;
     _totalAmount -= withdrawal;
@@ -112,7 +122,7 @@ void Account::displayStatus(void) const
 {
     _displayTimestamp();
     std::cout << "index:" << this->_accountIndex
-              << "; amount:" << this->_amount
-              << "; deposits:" << this->_nbDeposits
-              << "; withdrawals:" << this->_nbWithdrawals << std::endl;
+              << ";amount:" << this->_amount
+              << ";deposits:" << this->_nbDeposits
+              << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
