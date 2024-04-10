a1a141cbef49283b97e0721dd11bc318b77315cf
LeaYeh <smile2140@gmail.com>
2024-04-10 15:37:37 +0200

    fix: Remote forbidden container and handle unicode as well

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 14:15:38 by lyeh              #+#    #+#             */
-/*   Updated: 2024/04/10 11:59:15 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/04/10 15:34:18 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,21 +38,65 @@ void PhoneBook::_add_operation(void)
     this->_add_contact(first_name, last_name, nickname, phone_number, darkest_secret);
 }
 
+/*
+1-byte sequences: 0xxxxxxx (ASCII characters)
+2-byte sequences: 110xxxxx 10xxxxxx
+3-byte sequences: 1110xxxx 10xxxxxx 10xxxxxx
+4-byte sequences: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+0xC0:             11000000
+0x80:             10000000
+*/
+int PhoneBook::_get_visual_width(const std::string& text)
+{
+    int width = 0;
+
+    for (unsigned char c : text)
+    {
+        if ((c & 0xC0) != 0x80) // If the byte is not a continuation byte
+            width++;
+    }
+    return width;
+}
+
 std::string PhoneBook::_format_cell_text(const std::string &text)
 {
-    if (text.length() > _MAX_TEXT_LENGTH)
-        return text.substr(0, _MAX_TEXT_LENGTH - 1) + ".";
+    int len = 0;
+    int byte_pos = 0;
+
+    for (unsigned char c : text)
+    {
+        if ((c & 0xC0) != 0x80) // If the byte is not a continuation byte
+        {
+            if (len == _MAX_TEXT_LENGTH - 1)
+            {
+                if (byte_pos < text.length() - 1)
+                    return text.substr(0, byte_pos) + ".";
+                return text.substr(0, byte_pos);
+            }
+            len++;
+        }
+        byte_pos++;
+    }
+    // if (text.length() > _MAX_TEXT_LENGTH)
+    //     return text.substr(0, _MAX_TEXT_LENGTH - 1) + ".";
     return text;
 }
 
 std::string PhoneBook::_format_row_info(const Contact &contact)
 {
     std::stringstream ss;
+    std::string first_name = this->_format_cell_text(contact.get_first_name());
+    std::string last_name = this->_format_cell_text(contact.get_last_name());
+    std::string nickname = this->_format_cell_text(contact.get_nickname());
+    int extend_width_fn = first_name.length() - this->_get_visual_width(first_name);
+    int extend_width_ln = last_name.length() - this->_get_visual_width(last_name);
+    int extend_width_nn = nickname.length() - this->_get_visual_width(nickname);
 
     ss << std::setw(_MAX_TEXT_LENGTH) << contact.get_index() << "|"
-       << std::setw(_MAX_TEXT_LENGTH) << this->_format_cell_text(contact.get_first_name()) << "|"
-       << std::setw(_MAX_TEXT_LENGTH) << this->_format_cell_text(contact.get_last_name()) << "|"
-       << std::setw(_MAX_TEXT_LENGTH) << this->_format_cell_text(contact.get_nickname());
+       << std::setw(_MAX_TEXT_LENGTH + extend_width_fn) << first_name << "|"
+       << std::setw(_MAX_TEXT_LENGTH + extend_width_ln) << last_name << "|"
+       << std::setw(_MAX_TEXT_LENGTH + extend_width_nn) << nickname;
 
     return ss.str();
 }
--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: lyeh <lyeh@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 14:14:43 by lyeh              #+#    #+#             */
-/*   Updated: 2024/04/06 11:13:32 by lyeh             ###   ########.fr       */
+/*   Updated: 2024/04/10 15:18:05 by lyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ private:
     static const int _MAX_CONTACT = 8;
     static const int _MAX_TEXT_LENGTH = 10;
     int _contact_count;
-    std::array<Contact, _MAX_CONTACT> _contact_array;
+    Contact _contact_array[_MAX_CONTACT];
 
     void _add_contact(const std::string first_name,
                       const std::string last_name,
@@ -35,6 +35,7 @@ private:
     void _print_contact_list(void);
     void _add_operation(void);
     bool _search_operation(void);
+    int _get_visual_width(const std::string& text);
     int _handle_input_text(const std::string input);
     std::string _format_cell_text(const std::string& text);
     std::string _format_row_info(const Contact& contact);
