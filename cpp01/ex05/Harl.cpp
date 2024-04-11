e120a7bc8861721e99119c922dfc86069d21a2db
LeaYeh <smile2140@gmail.com>
2024-04-11 10:41:54 +0200

    fix: Change backend logic to fix the requirement

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -7,55 +7,59 @@ Harl::~Harl(void) {};
 
 Harl::Level Harl::_mapStringToLevel(const std::string &level)
 {
-    if (level == "debug") return DEBUG;
-    else if (level == "info") return INFO;
-    else if (level == "warning") return WARNING;
-    else if (level == "error") return ERROR;
-    else return UNKNOWN;
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == levels[i])
+            return static_cast<Level>(i);
+    }
+    return UNKNOWN;
 }
 
 void Harl::debug(void)
 {
-    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ DEBUG ]";
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl;
+    std::cout << "I really do!" << std::endl;
+    std::cout << std::endl;
 }
 
 void Harl::info(void)
 {
-    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ INFO ]";
-    std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon cost more money." << std::endl;
+    std::cout << "You don’t put enough! If you did I would not have to ask for it!" << std::endl;
+    std::cout << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ WARNING ]";
-    std::cout << "I think I’ve lost my appetite. I’m going to cancel my order." << std::endl;
+    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ WARNING ]" << std::endl;
+    std::cout << "I think I’ve lost my appetite." << std::endl;
+    std::cout << "I’m going to cancel my order." << std::endl;
+    std::cout << std::endl;
 }
 
 void Harl::error(void)
 {
-    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ ERROR ]";
-    std::cout << "Sorry, I can’t take your order. The kitchen is closed." << std::endl;
+    std::cout << std::setw(_PREFIX_LENGTH) << std::left << "[ ERROR ]" << std::endl;;
+    std::cout << "Sorry, I can’t take your order." << std::endl;
+    std::cout << "The kitchen is closed." << std::endl;
+    std::cout << std::endl;
 }
 
 void Harl::complain(std::string const &level)
 {
-    switch (this->_mapStringToLevel(level))
+    void (Harl::*complaints[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    Level lvl = _mapStringToLevel(level);
+
+    for (int i = 0; i < 4; i++)
     {
-        case DEBUG:
-            debug();
-            break;
-        case INFO:
-            info();
-            break;
-        case WARNING:
-            warning();
-            break;
-        case ERROR:
-            error();
-            break;
-        default:
-            std::cout << "Unknown level" << std::endl;
-            break;
+        if (lvl == i)
+        {
+            (this->*complaints[i])();
+            return;
+        }
     }
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -4,10 +4,11 @@ int main(void)
 {
     Harl harl;
 
-    harl.complain("debug");
-    harl.complain("info");
-    harl.complain("warning");
-    harl.complain("error");
-    harl.complain("unknown");
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("UNKNOWN");
+
     return 0;
 }
