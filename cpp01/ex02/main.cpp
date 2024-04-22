3f1f266f8ab4be3b51572201d6307e955321ac5c
LeaYeh <smile2140@gmail.com>
2024-04-22 16:32:38 +0200

    fix: Align the text with the subject requirement

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    std::string     str = "string context abc";
-    std::string*    stringPTR = &str;
-    std::string&    stringREF = str;
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
 
     std::cout << "string(address): " << &str << std::endl;
     std::cout << "stringPTR(address): " << stringPTR << std::endl;
