4bd3b52bc3f0a3e48d49d0c5eee5a8ec64ed0c61
LeaYeh <smile2140@gmail.com>
2024-04-26 17:14:56 +0200

    fix: Make copy constructor really independant

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef __FIXED_HPP
+#define __FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+public:
+    Fixed();
+    Fixed(const int value);
+    Fixed(const float value);
+    Fixed(const Fixed &other);
+    ~Fixed();
+
+    Fixed &operator=(const Fixed &other);
+
+    float toFloat(void) const;
+    int toInt(void) const;
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+private:
+    int _value;
+    static const int FRACT_BITS = 8;
+};
+
+#endif // __FIXED_HPP
