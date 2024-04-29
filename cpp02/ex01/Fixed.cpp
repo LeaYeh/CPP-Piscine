6ba93fb11a2acd6be21ec6525318f33a51ccd91c
Lea Yeh <lea.yeh.ml@gmail.com>
2024-04-29 22:59:38 +0200

    fix: Use roundf to get more accuracy result

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -5,14 +5,14 @@ Fixed::Fixed() : _value(0)
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const int value) : _value(value << _FRACT_BITS)
+Fixed::Fixed(const int value) : _value(value << Fixed::_FRACT_BITS)
 {
     std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float value)
 {
-    this->_value = int(value) << _FRACT_BITS;
+    this->_value = roundf(value * (1 << Fixed::_FRACT_BITS));
     std::cout << "Float constructor called" << std::endl;
 }
 
@@ -31,7 +31,7 @@ Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Assignation operator called" << std::endl;
     if (this != &other)
-        _value = other.getRawBits();
+        this->_value = other.getRawBits();
     return *this;
 }
 
@@ -45,7 +45,7 @@ std::ostream &operator<<(std::ostream &os, const Fixed &num)
 int Fixed::getRawBits(void) const
 {
     std::cout << "getRawBits member function called" << std::endl;
-    return _value;
+    return this->_value;
 }
 
 void Fixed::setRawBits(int const raw)
@@ -56,10 +56,10 @@ void Fixed::setRawBits(int const raw)
 
 int Fixed::toInt(void) const
 {
-    return _value >> _FRACT_BITS;
+    return this->_value >> Fixed::_FRACT_BITS;
 }
 
 float Fixed::toFloat(void) const
 {
-    return _value / (1 << _FRACT_BITS);
+    return (float)((float)(this->_value) / (float)(1 << Fixed::_FRACT_BITS));
 }
