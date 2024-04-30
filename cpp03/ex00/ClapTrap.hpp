641f3ccd60c9a4ed9d5746955fc087ee4397663c
Lea Yeh <lea.yeh.ml@gmail.com>
2024-04-30 22:56:33 +0200

    feat: Implement cpp03ex00

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,33 @@
+#ifndef __CLAPTRAP_HPP__
+#define __CLAPTRAP_HPP__
+
+#include <iostream>
+
+class ClapTrap
+{
+public:
+    ClapTrap();
+    ClapTrap(const std::string &name);
+    ClapTrap(const ClapTrap &other);
+    ~ClapTrap();
+
+    ClapTrap &operator=(const ClapTrap &other);
+
+    const std::string& getName(void) const;
+    int getHitPoint(void) const;
+    int getEnergyPoint(void) const;
+    int getAttackDamage(void) const;
+    void printStatus(void) const;
+    bool checkAction(const std::string &action) const;
+    void attack(const std::string &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+
+private:
+    std::string _name;
+    int _hitPoint;
+    int _energyPoint;
+    int _attackDamage;
+};
+
+#endif
---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,23 @@
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap a("Alex");
+	ClapTrap b("Bob");
+	ClapTrap c(a);
+	ClapTrap d;
+	d = b;
+
+	a.attack(b.getName());
+	a.printStatus();
+	a.takeDamage(4);
+	a.beRepaired(4);
+	a.printStatus();
+	a.takeDamage(50);
+	a.attack(b.getName());
+
+	for (int i = 0; i < 10; ++i) {
+		b.beRepaired(10);
+	}
+	b.beRepaired(10);
+}
