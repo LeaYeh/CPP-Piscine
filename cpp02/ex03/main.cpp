45fc90a7848bf0b896063708a0936970d537bdd7
LeaYeh <smile2140@gmail.com>
2024-05-01 13:04:50 +0200

    fix: Add more example to demo

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -2,11 +2,14 @@
 
 int main(void)
 {
-	Point a(Fixed(0), Fixed(0));
-	Point b(Fixed(0), Fixed(1));
-	Point c(Fixed(1), Fixed(0));
-	
+	Point a(Fixed(2), Fixed(8));
+	Point b(Fixed(8), Fixed(4));
+	Point c(Fixed(3), Fixed(1));
+	Point p(Fixed(3), Fixed(2));
+
 	print_grid(a, b, c);
+
+	std::cout << p << (bsp(a, b, c, Point(Fixed(3), Fixed(2))) ? ", is inside of the triangle\n" : ", is outside of the triangle\n");
 
 	return (0);
 }
