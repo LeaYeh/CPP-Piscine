cb5ee53c618af5a332844ced273ff87e9cebc880
LeaYeh <smile2140@gmail.com>
2024-05-03 14:54:12 +0200

    feat: Implement the wrong class for demo cpp04ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -1,22 +1,42 @@
 #include "Animal.hpp"
+#include "WrongAnimal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main(void)
 {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    
+    {
+        const Animal *meta = new Animal();
+        const Animal *j = new Dog();
+        const Animal *i = new Cat();
+
+        std::cout << "==========================\n";
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
+        meta->makeSound();
+        i->makeSound();
+        j->makeSound();
+        std::cout << "==========================\n";
+        delete meta;
+        delete i;
+        delete j;
+        std::cout << "==========================\n";
+    }
+    {
+        const WrongAnimal *meta = new WrongAnimal();
+        const WrongAnimal *i = new WrongCat();
+
+        std::cout << "==========================\n";
+        std::cout << i->getType() << " " << std::endl;
+        meta->makeSound();
+        i->makeSound();
+
+        std::cout << "==========================\n";
+        delete meta;
+        delete i;
+        std::cout << "==========================\n";
+    }
     std::cout << "==========================\n";
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    meta->makeSound();
-    i->makeSound(); //will output the cat sound! j->makeSound(); meta->makeSound(); ...
-    std::cout << "==========================\n";
-    delete meta;
-    delete i;
-    delete j;
-    std::cout << "==========================\n";
-    return 0; 
+    return 0;
 }
