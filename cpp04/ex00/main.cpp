708455234db0a7c5281c2f930e2f778fbdbd5fd7
LeaYeh <smile2140@gmail.com>
2024-05-05 11:15:47 +0200

    fix: Fix typo

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -48,10 +48,10 @@ void testAnimal()
     std::cout << "--- TEST Detault Constructor ---\n";
     Animal a;
     a.printInfo();
-    std::cout << "\n--- TEST Cpoy Constructor ---\n";
+    std::cout << "\n--- TEST Copy Constructor ---\n";
     Animal b(a);
     b.printInfo();
-    std::cout << "\n--- TEST Cpoy Operator ---\n";
+    std::cout << "\n--- TEST Copy Operator ---\n";
     a = b;
     a.printInfo();
 }
@@ -62,10 +62,10 @@ void testWrongAnimal()
     std::cout << "--- TEST Detault Constructor ---\n";
     WrongAnimal a;
     a.printInfo();
-    std::cout << "\n--- TEST Cpoy Constructor ---\n";
+    std::cout << "\n--- TEST Copy Constructor ---\n";
     WrongAnimal b(a);
     b.printInfo();
-    std::cout << "\n--- TEST Cpoy Operator ---\n";
+    std::cout << "\n--- TEST Copy Operator ---\n";
     a = b;
     a.printInfo();
 }
@@ -76,10 +76,10 @@ void testCat()
     std::cout << "--- TEST Detault Constructor ---\n";
     Cat a;
     a.printInfo();
-    std::cout << "\n--- TEST Cpoy Constructor ---\n";
+    std::cout << "\n--- TEST Copy Constructor ---\n";
     Cat b(a);
     b.printInfo();
-    std::cout << "\n--- TEST Cpoy Operator ---\n";
+    std::cout << "\n--- TEST Copy Operator ---\n";
     Cat d;
     d = a;
     d.printInfo();
@@ -95,10 +95,10 @@ void testWrongCat()
     std::cout << "--- TEST Detault Constructor ---\n";
     WrongCat a;
     a.printInfo();
-    std::cout << "\n--- TEST Cpoy Constructor ---\n";
+    std::cout << "\n--- TEST Copy Constructor ---\n";
     WrongCat b(a);
     b.printInfo();
-    std::cout << "\n--- TEST Cpoy Operator ---\n";
+    std::cout << "\n--- TEST Copy Operator ---\n";
     WrongCat d;
     d = a;
     d.printInfo();
@@ -114,10 +114,10 @@ void testDog()
     std::cout << "--- TEST Detault Constructor ---\n";
     Dog a;
     a.printInfo();
-    std::cout << "\n--- TEST Cpoy Constructor ---\n";
+    std::cout << "\n--- TEST Copy Constructor ---\n";
     Dog b(a);
     b.printInfo();
-    std::cout << "\n--- TEST Cpoy Operator ---\n";
+    std::cout << "\n--- TEST Copy Operator ---\n";
     Dog d;
     d = a;
     d.printInfo();
