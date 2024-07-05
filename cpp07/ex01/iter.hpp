6782fb89a8f85b30e7231a2e71bb505e9fddda39
LeaYeh <smile2140@gmail.com>
2024-07-05 22:54:52 +0200

    feat: Implement cpp07 ex01

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+template <typename T, typename Function>
+void iter(T &array, int length, Function func);
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "iter.hpp"
+
+template <typename T>
+void printElementWithNewLine(T &value)
+{
+    std::cout << value << std::endl;
+}
+
+template <typename T>
+void printElement(T &value)
+{
+    std::cout << value << " ";
+}
+
+template <typename T, typename Function>
+void iter(T &array, int length, Function func)
+{
+    for (int i = 0; i < length; i++)
+        func(array[i]);
+}
+
+int main(void)
+{
+    {
+        int arr[] = {1, 2, 3, 4};
+
+        std::cout << "\nPrint out each element with <int>\n";
+        iter(arr, 4, printElement<int>);
+        std::cout << "\nPrint out each with newline element with <int>\n";
+        iter(arr, 4, printElementWithNewLine<int>);
+    }
+    {
+        char arr[] = {'a', 'b', 'c', 'd'};
+
+        std::cout << "\nPrint out each element with <char>\n";
+        iter(arr, 4, printElement<char>);
+        std::cout << "\nPrint out each with newline element with <char>\n";
+        iter(arr, 4, printElementWithNewLine<char>);
+    }
+    {
+        float arr[] = {1.1f, 2.2f, 3.3f, 4.4f};
+
+        std::cout << "\nPrint out each element with <float>\n";
+        iter(arr, 4, printElement<float>);
+        std::cout << "\nPrint out each with newline element with <float>\n";
+        iter(arr, 4, printElementWithNewLine<float>);
+    }
+}
