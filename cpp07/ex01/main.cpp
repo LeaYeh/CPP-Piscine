19f7ce8d3db8376c64eef9092d32e5e6535645c2
LeaYeh <smile2140@gmail.com>
2024-07-06 21:14:23 +0200

    fix: Check the array size before oter the elements

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iterator>
 #include "iter.hpp"
 
 template <typename T>
@@ -13,9 +14,17 @@ void printElement(T &value)
     std::cout << value << " ";
 }
 
+template <typename T, int N>
+int arraySize(T (&)[N])
+{
+    return N;
+}
+
 template <typename T, typename Function>
 void iter(T &array, int length, Function func)
 {
+    if (length > arraySize(array))
+        throw (std::range_error("Cannot iter out of array size."));
     for (int i = 0; i < length; i++)
         func(array[i]);
 }
@@ -45,5 +54,18 @@ int main(void)
         iter(arr, 4, printElement<float>);
         std::cout << "\nPrint out each with newline element with <float>\n";
         iter(arr, 4, printElementWithNewLine<float>);
+    }
+    {
+        int arr[] = {1, 2, 3, 4};
+
+        try
+        {
+            std::cout << "\nPrint out each element with <int>\n";
+            iter(arr, 10, printElement<int>);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
     }
 }
