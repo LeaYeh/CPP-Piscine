a5b15f4b76c09ae173a5c024aaeb6c4aec87607b
LeaYeh <smile2140@gmail.com>
2024-07-06 19:18:08 +0200

    feat: Add more test case for cpp07 ex02

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -46,6 +46,24 @@ int main(void)
         std::cout << "array_int: ";
         array_int.printElements();
     }
+    {
+        std::cout << std::endl;
+        Array<int> array_int(5);
+
+        for (int i = 0; i < 5; i++)
+            array_int[i] = 0;
+        array_int.printElements();
+        array_int[1] = 5;
+        array_int.printElements();
+        try
+        {
+            std::cout << "array_int[10]: " << array_int[10];
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
 
     return (0);
 }
