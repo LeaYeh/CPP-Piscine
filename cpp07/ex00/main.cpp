a0b1742c4ec89e874cdb26f36fdfb6eb486151c4
LeaYeh <smile2140@gmail.com>
2024-07-16 19:28:54 +0200

    chore: Add ignore rule into cpp07

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,27 +1,5 @@
 #include "whatever.hpp"
 
-template <typename T>
-void swap(T &a, T &b)
-{
-    T tmp;
-
-    tmp = a;
-    a = b;
-    b = tmp;
-}
-
-template <typename T>
-T min(T a, T b)
-{
-    return (a < b ? a : b);
-}
-
-template <typename T>
-T max(T a, T b)
-{
-    return (a > b ? a : b);
-}
-
 int main(void)
 {
     int a = 2;
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -1,11 +1,46 @@
 #pragma once
 #include <iostream>
 
-template <typename T>
-void swap(T &a, T &b);
+/*
+1. the implementation of the function template should be in the header file or the tpp file
+2. Should put the reference in min and max function to avoid copying the value, sometimes it is not possible to copy the value
+3. Always use the reference to avoid copying the value
+4. Always use the const reference if it is not necessary to change the value
+5. move semantics since c++11
+    - move assignment
+    ```example
+        template <typename T>
+        void swap(T &a, T &b)
+        {
+            move semantics since c++11
+            T tmp = std::move(a);
+            a = std::move(b);
+            b = std::move(tmp);
+        }
+    ```
+6. namespace
+    - The default namespace is the global namespace
+    - Use the namespace to avoid the conflict of the function name
+*/
 
 template <typename T>
-T min(T a, T b);
+void swap(T &a, T &b)
+{
+    T tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
 
 template <typename T>
-T max(T a, T b);
+T& min(T &a, T &b)
+{
+    return (a < b ? a : b);
+}
+
+template <typename T>
+T& max(T &a, T &b)
+{
+    return (a > b ? a : b);
+}
