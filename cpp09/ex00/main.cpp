5a0af56a986e0b8a13fc4e4d9f4c18e215097dd2
LeaYeh <smile2140@gmail.com>
2024-07-24 17:03:47 +0200

    fix: Change the error message

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        std::cerr << "usage: ./btc [input_file]" << std::endl;
+        std::cerr << "Error: could not open file." << std::endl;
         return (1);
     }
     try
